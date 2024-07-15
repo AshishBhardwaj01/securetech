5c9cace3cdd083d5923248f4dec1c1889e02866d
Ashish Bhardwaj <ashishbhardwaj.rei@gmail.com>
2024-07-15 16:21:54 +0530

    "Refactored HeroContent component: removed unnecessary divs, simplified CSS classes, and rearranged layout and styling."

--- src/components/HomeBg.jsx
+++ src/components/HomeBg.jsx
@@ -43,66 +43,57 @@
 // };
 
 // export default MySection;
+import React from "react";
 import PropTypes from "prop-types";
 
 const HeroContent = ({ className = "" }) => {
   return (
     <div
-      className={`self-stretch h-[49.688rem] lg:h-[20rem] flex flex-row flex-wrap items-center justify-center py-[6.25rem]  box-border relative gap-[0rem_0.625rem] bg-[url('/Banner.gif')] bg-cover bg-no-repeat bg-[top] max-w-full text-left text-[4.375rem] lg:text-lg-5 lg:text- text-orangered font-poppins ${className}`}
+      className={`relative flex flex-col contrast-125 w-screen h-screen items-start lg:items-center lg:justify-center justify-start py-10 md:py-5 lg:px-2 bg-[url('/Banner.gif')] bg-cover bg-no-repeat bg-top text-left lg:text-center font-poppins ${className}`}
     >
-      <div className="h-[48.813rem] w-[120rem] relative bg-[url('Banner.gif')] bg-cover bg-no-repeat bg-[top] hidden max-w-full z-[0]" />
-           <div className="flex flex-col">
-            <h1 className="!m-[0] w-[50.563rem] lg:w-auto absolute lg:relative 2xl:top-[7.313rem] xl:top-[7.313rem] lg:top-[2rem] 2xl:left-[7.563rem] xl:left-[7.563rem] text-inherit font-normal font-inherit flex items-center  ">
-               <span className="w-full lg:w-auto ">
-                 <span className="text-orange-500">Elevating</span>
-                 <span className="text-orange-500">{` `}</span>
-                 <span className="text-orange-500">Experiences</span>
-                 <span className="text-orange-500">{`, `}</span>
-                 <span className="text-gray-800 whitespace-pre-wrap">
-                   {" "}
-                   Redefining Technology
-                 </span>
-               </span>
-             </h1>
-             <div className="w-[39.375rem] lg:w-auto absolute !m-[0] top-[23.531rem] lg:top-[12rem] left-[7.563rem] text-[1.25rem] lg:text-sm text-white flex items-center z-[1] ">
-               We provide tailored, cutting-edge audio and video solutions. Whether
-               enhancing home entertainment, optimizing business conferencing, or
-               creating a state-of-the-art media room, our experts are here to guide
-               you.
-             </div>
-             <div className="flex flex-row ">
-             <a href="/AboutUs" className="cursor-pointer no-underline rounded-xl px-[1rem] py-3 bg-white   text-orange-500 hover:bg-orange-500 hover:text-white w-[13.813rem] !m-[0] absolute top-[35.875rem] left-[7.563rem] overflow-hidden shrink-0 flex flex-row items-end justify-start box-border gap-[0.381rem] whitespace-nowrap z-[1] ">
-   <div className="w-[9.581rem] relative pb-1.5 text-[1.5rem] leading-[1.444rem] font-poppins flex items-center justify-center shrink-0">
-     Learn more
-   </div>
-   <img
-     className="m-0 h-8 w-8 relative transition-opacity duration-300 opacity-100 "
-     loading="lazy"
-     alt=""
-     src="arow-unscreen.gif"
-   />
- </a>
-
-<a href="/Contactus" className="!m-[0] h-[1.444rem] w-[9.581rem] absolute top-[36.0rem] lg:top-[15rem] no-underline left-[26.038rem] text-[1.5rem] leading-[5.512rem] font-normal font-inherit p-3 text-white hover:text-orange-500 text-center flex items-center justify-center z-[1] mq450:text-[1.188rem] mq450:leading-[4.438rem] hover:border-orange-500 group">
-  Contact Us
-  <span className="absolute top-0 left-0 w-0 h-0.5 bg-white transition-all duration-500 ease-out group-hover:w-full"></span>
-  <span className="absolute top-0 left-0 h-0 w-0.5 bg-white transition-all duration-500 ease-out group-hover:h-full"></span>
-  <span className="absolute bottom-0 right-0 w-0 h-0.5 bg-white transition-all duration-500 ease-out group-hover:w-full"></span>
-  <span className="absolute bottom-0 right-0 h-0 w-0.5 bg-white transition-all duration-500 ease-out group-hover:h-full"></span>
+      <div className="container items-start justify-start mx-auto px-4 py-40 lg:py-20 lg:px-1">
+        <h1 className="text-8xl md:text-6xl xl:text-7xl font-normal mb-6  lg:m-0">
+          <span className="text-orange-500 lg:text-2xl">Elevating Experiences, </span>
+          <br className=" md:block" />
+          <span className="text-gray-800 lg:text-2xl">Redefining Technology</span>
+        </h1>
+        <p className="text-white text-left ml-2 pb-10 text-mid md:text-lg lg:text-base max-w-2xl mx-auto mb-8 lg:mb-12">
+          We provide tailored, cutting-edge audio and video solutions. Whether
+          enhancing home entertainment, optimizing business conferencing, or
+          creating a state-of-the-art media room, our experts are here to guide
+          you.
+        </p>
+        <div className="flex flex-row sm:flex-row lg:pl-2  justify-start items-center gap-4">
+          <a
+            href="/AboutUs"
+  className="inline-flex items-center justify-center lg:text-base no-underline px-6 py-3 bg-white text-orange-500 hover:bg-orange-500 hover:text-white rounded-xl text-lg font-poppins transition-colors duration-300"
+>
+  <span>Learn more</span>
+  <img
+    className="h-6 w-6 ml-2"
+    loading="lazy"
+    alt=""
+    src="arow-unscreen.gif"
+  />
 </a>
+          <a href="/Contactus" className="relative px-8 py-3 text-lg lg:text-base no-underline hover:text-orange-500 text-center text-white flex items-center justify-center hover:border-orange-500 group">
+            Contact Us
+            <span className="absolute top-0 left-0 w-0 h-0.5 bg-white transition-all duration-500 ease-out group-hover:w-full"></span>
+            <span className="absolute top-0 left-0 h-0 w-0.5 bg-white transition-all duration-500 ease-out group-hover:h-full"></span>
+            <span className="absolute bottom-0 right-0 w-0 h-0.5 bg-white transition-all duration-500 ease-out group-hover:w-full"></span>
+            <span className="absolute bottom-0 right-0 h-0 w-0.5 bg-white transition-all duration-500 ease-out group-hover:h-full"></span>
+          </a>       
+        </div>
+      </div>
+    </div>
+  );
+};
 
-             </div>
-             </div>
-     </div>
-    
-   );
- };
+HeroContent.propTypes = {
+  className: PropTypes.string,
+};
 
- HeroContent.propTypes = {
-   className: PropTypes.string,
- };
-
- export default HeroContent;
+export default HeroContent;
 
 // import React from "react";
 // import PropTypes from "prop-types";
